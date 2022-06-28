40540b926f26ec7f629632e5769d98c3d0ed5180
chehin <chehinjuan@gmail.com>
2022-06-28 16:42:37 -0300

    alert numeric price

--- src/app/productos/productos.page.ts
+++ src/app/productos/productos.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-productos',
@@ -12,17 +14,29 @@ export class ProductosPage implements OnInit {
 
   productos = []
 
+  habilitarCalcular = false;
+
   name: string;
-  precio: string;
+  precio: number;
+  total = 0;
 
   message: string;
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
   }
 
-  abrirModal() {
+  async presentAlert(mensaje: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alerta',
+      subHeader: 'Mensaje',
+      message: mensaje,
+      buttons: ['OK']
+    });
+
+    await alert.present();
 
   }
 
@@ -31,8 +45,21 @@ export class ProductosPage implements OnInit {
   }
 
   confirm() {
+    // Chequear que el precio sea numerico
+    if(!this.isNumberic(this.precio))
+    {
+      this.presentAlert('El precio debe ser numerico')
+      return;
+    }
+
     this.productos.push({ nombre: this.name,precio : this.precio});
     this.modal.dismiss(this.name, 'confirm');
+    this.calcular(this.precio);
+  }
+
+
+  calcular(precio: number) {
+    this.total += precio;
   }
 
   onWillDismiss(event: Event) {
@@ -40,5 +67,13 @@ export class ProductosPage implements OnInit {
     if (ev.detail.role === 'confirm') {
       this.message = `Hello, ${ev.detail.data}!`;
     }
+  }
+
+
+  isNumberic(value: string | number): boolean
+  {
+   return ((value != null) &&
+           (value !== '') &&
+           !isNaN(Number(value.toString())));
   }
 }
