76f7fc2146ea7e56ec9c1cdb2d4f9647a845c524
chehin <chehinjuan@gmail.com>
2022-06-29 21:16:34 -0300

    install sql-lite

--- src/app/configuraciones/configuraciones.page.ts
+++ src/app/configuraciones/configuraciones.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @Component({
   selector: 'app-configuraciones',
@@ -6,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfiguracionesPage implements OnInit {
 
-  constructor() { }
+  constructor(private sqlite: SQLite) { }
 
   ngOnInit() {
+    this.crearBD();
   }
+
+  crearBD(){
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+      .then((db: SQLiteObject) => {
+
+
+        db.executeSql('create table configuraciones(cant_cuotas int)', [])
+          .then(() => console.log('Executed SQL'))
+          .catch(e => console.log(e));
+      })
+      .catch(e => console.log(e));
+  }
+
 
 }
