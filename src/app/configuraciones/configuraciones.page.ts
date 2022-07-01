427d3e1096ae146fa5cc7347220bdc84fea21b3f
chehin <chehinjuan@gmail.com>
2022-07-01 17:47:48 -0300

    num cuotas gestionable

--- src/app/configuraciones/configuraciones.page.ts
+++ src/app/configuraciones/configuraciones.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@capacitor/storage';
 
 @Component({
   selector: 'app-configuraciones',
@@ -8,7 +9,27 @@ export class ConfiguracionesPage implements OnInit {
 
   constructor() { }
 
+  cantCuotasStorage: string;
+
   ngOnInit() {
+    this.dameCuotasStorage();
   }
+
+  async dameCuotasStorage()
+  {
+    const { value } = await Storage.get({ key: 'num-cuotas' });
+    this.cantCuotasStorage = value;
+  }
+
+  async modificarCantCuotas(event: Event)
+  {
+    await Storage.set({
+      key: 'num-cuotas',
+      value: String(this.cantCuotasStorage)
+    });
+  }
+
+
+
 
 }
